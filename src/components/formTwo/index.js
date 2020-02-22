ce7e9d57e94fe0e4b9bea4d808c727ed13456ddc
sleekcharly <c_ukasoanya@yahoo.com>
2020-02-22 05:59:31 +0100

    used formik to handle forms.

--- src/components/formTwo/index.js
+++ src/components/formTwo/index.js
@@ -1,61 +1,104 @@
 import React,{ Component } from 'react';
+import { Formik } from 'formik';
 
 class FormTwo extends Component {
 
     state = {
-        maxAge:80
+        maxAge:81
+    }
+
+    generateOptions = () => {
+        const ageArray = [];
+        for(let i = 1; i < this.state.maxAge; i++){
+            ageArray.push(i);
+        }
+
+        return ageArray.map((value, i) => {
+            return (
+                <option key={i} value={value}>{value}</option>
+            )
+        })
     }
 
 
     render(){
         return(
             <>
-                <form>
-                    <div className="form-group">
-                        <label>Name</label>
-                        <input 
-                            type="text"
-                            className="form-control"
-                            name="name_input"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Lastname</label>
-                        <input 
-                            type="text"
-                            className="form-control"
-                            name="lastname_input"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Age</label>
-                        <select
-                            name="age_input"
-                            className="form-control" 
-                        >
-                           
-                        </select>
-                    </div>
-                    
-                    <div className="form-group">
-                        <label>Enter your message here</label>
-                        <textarea 
-                            rows="3"
-                            placeholder="Add your message here..."
-                            className="form-control"
-                        ></textarea>
-                    </div>
-                    
-                    <button 
-                        type="submit"
-                        className="btn btn-primary"
-                        onClick={(event)=>this.submitForm(event)}
-                        disabled={this.state.loading}
-                    >
-                        Submit
-                    </button>
 
-                </form>
+                <Formik 
+                    initialValues={{ name: '', lastname: '', age: '', message: 'Enter your message' }}
+                >
+                    { ({
+                        values,
+                        errors,
+                        touched,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        isSubmitting,
+                    }) => (
+                        <form>
+                            <div className="form-group">
+                                <label>Name</label>
+                                <input 
+                                    type="text"
+                                    className="form-control"
+                                    name="name"
+                                    value={values.name}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Lastname</label>
+                                <input 
+                                    type="text"
+                                    className="form-control"
+                                    name="lastname"
+                                    value={values.lastname}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Age</label>
+                                <select
+                                    name="age"
+                                    className="form-control"
+                                    value={values.age}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur} 
+                                >
+                                {this.generateOptions()}
+                                </select>
+                            </div>
+                            
+                            <div className="form-group">
+                                <label>Enter your message here</label>
+                                <textarea 
+                                    rows="3"
+                                    placeholder="Add your message here..."
+                                    className="form-control"
+                                    name="message"
+                                    value={values.message}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                ></textarea>
+                            </div>
+                            
+                            <button 
+                                type="submit"
+                                className="btn btn-primary"
+                                onClick={(event)=>this.submitForm(event)}
+                                disabled={this.state.loading}
+                            >
+                                Submit
+                            </button>
+        
+                        </form>
+                    )}
+                </Formik>
+
             </>
         )
     }
